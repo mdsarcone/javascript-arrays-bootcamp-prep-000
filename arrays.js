62caa8c31c834193dddfecdb64e9b715d8c58ae4
Mark Sarcone <mdsarcone@gmail.com>
2017-09-02 15:19:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
-function addElementToEndOfArray(array,element) {
+function addElementToBeginningOfArray(array,element) {
   var newArray = array.unshift(element)
   return newArray
 }
